8fd9b95f156c99621d5c334ba401e05a80ac195a
codeftsaikat <soikatdey18@gmail.com>
2024-10-18 12:23:57 -0700

    Set up skills section framework with initial layout and sample skills content

---
+++ components/Home/Skills/Skills.tsx
@@ -0,0 +1,12 @@
+import SectionHeading from '@/components/Helper/SectionHeading'
+import React from 'react'
+
+const Skills = () => {
+  return (
+    <div className='pt-16 pb-16 bg-[#0f0715]'>
+        <SectionHeading>My Skills</SectionHeading>
+    </div>
+  )
+}
+
+export default Skills
