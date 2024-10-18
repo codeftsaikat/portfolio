1724f6d5b3054f757e6ce1b346e2120a77b2ad84
codeftsaikat <soikatdey18@gmail.com>
2024-10-17 20:58:35 -0700

    Implement the navbar section in the portfolio project

---
+++ components/Home/Home.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div className='overflow-hidden bg-green-200'>Home</div>
+  )
+}
+
+export default Home
---
+++ components/Home/Navbar/Nav.tsx
@@ -0,0 +1,42 @@
+import React from "react";
+import Image from "next/image";
+import { navLinks } from "@/constant/constant";
+import Link from "next/link";
+import { HiBars3BottomRight } from "react-icons/hi2";
+const Nav = () => {
+  return (
+    <div className="fixed h-[12vh] z-[10] bg-blue-950 w-full">
+      <div className="flex items-center h-full justify-between w-[95%] sm:w-[90%] xl:w-[80%] mx-auto">
+        {/* logo */}
+        <Image
+          src="/images/logo.png"
+          width={170}
+          height={170}
+          alt="logo"
+          className="ml-[-1.5rem] sm:ml-0"
+        />
+        <div className="flex items-center space-x-10">
+          {/* nav links */}
+          <div className="hidden lg:flex space-x-8 items-center">
+            {navLinks.map((navLink) => {
+              return (
+                <Link href={navLink.url} key={navLink.id}>
+                  <p className="nav__links">{navLink.label}</p>
+                </Link>
+              );
+            })}
+          </div>
+          {/* button */}
+          <div className="flex items-center space-x-4">
+           <button className="px-8 py-3 md:px-10 text-blue-800 font-semibold sm:text-base bg-white hover:bg-gray-200 transition-all duration-200 rounded-lg">Hire Me</button>
+
+           {/* burger */}
+           <HiBars3BottomRight className="h-8 w-8 cursor-pointer text-white lg:hidden"/>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Nav;
