d1643a26ac3ee7bd506fef9d6c5af87a27bd8be0
codeftsaikat <soikatdey18@gmail.com>
2024-10-18 11:44:38 -0700

    Complete services section with detailed service descriptions, icons, and responsive grid layout

--- components/Home/Services/Services.tsx
+++ components/Home/Services/Services.tsx
@@ -1,14 +1,17 @@
 import SectionHeading from "@/components/Helper/SectionHeading";
 import { servicesData } from "@/data/data";
 import React from "react";
+import ServiceCard from "./ServiceCard/ServiceCard";
 
 const Services = () => {
   return (
     <div className="bg-[#0f0751] pt-16 pb-16">
       <SectionHeading>Services</SectionHeading>
-      <div>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-10 w-[80%] mx-auto items-center mt-20">
         {servicesData.map((service) => {
-          return <div key={service.id}></div>;
+          return <div key={service.id}>
+            <ServiceCard service={service}/>
+          </div>;
         })}
       </div>
     </div>
