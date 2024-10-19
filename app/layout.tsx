79f18c0ff6186a997e5657edab9afe7f5b4730ca
codeftsaikat <soikatdey18@gmail.com>
2024-10-19 10:31:58 -0700

    Added animation in this project

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import {Sora} from 'next/font/google'
 import "./globals.css";
 import ResponsiveNav from "@/components/Home/Navbar/ResponsiveNav";
+import ScrollToTop from "@/components/Helper/ScrollToTop";
 
 const font = Sora({
   weight:['100','200','300','400','500','600','700','800'],
@@ -19,12 +20,13 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" className="custom-scrollbar">
       <body
         className={font.className}
       >
         <ResponsiveNav/>
         {children}
+        <ScrollToTop/>
       </body>
     </html>
   );
