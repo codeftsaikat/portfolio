abfa8e8fab871a982932e4dca6777a64e8bf6c89
codeftsaikat <soikatdey18@gmail.com>
2024-10-18 21:24:13 -0700

    Initialize reviews section with layout structure, sample reviews, and responsive design

---
+++ components/Home/Reviews/Reviews.tsx
@@ -0,0 +1,12 @@
+import SectionHeading from '@/components/Helper/SectionHeading'
+import React from 'react'
+
+const Reviews = () => {
+  return (
+    <div className='pt-16 pb-16 bg-[#050701]'>
+        <SectionHeading>My Skills</SectionHeading>
+    </div>
+  )
+}
+
+export default Reviews
