5d3aec0cebb3f2a2e4581b96e173aa86fcb1d234
codeftsaikat <soikatdey18@gmail.com>
2024-10-19 00:03:20 -0700

    Finalize reviews section with customer feedback, styling enhancements, and mobile responsiveness

--- components/Home/Reviews/Reviews.tsx
+++ components/Home/Reviews/Reviews.tsx
@@ -1,10 +1,15 @@
 import SectionHeading from '@/components/Helper/SectionHeading'
 import React from 'react'
+import Slider from './Slider/Slider'
 
 const Reviews = () => {
   return (
     <div className='pt-16 pb-16 bg-[#050701]'>
         <SectionHeading>My Skills</SectionHeading>
+        <div className='w-[90%] sm:w-[90%] mx-auto mt-20'>
+            {/* slider */}
+            <Slider/>
+        </div>
     </div>
   )
 }
---
+++ components/Home/Reviews/Slider/Slider.tsx
@@ -0,0 +1,38 @@
+"use client";
+import { clientReviews } from "@/Data/data";
+import React from "react";
+import Carousel from "react-multi-carousel";
+import "react-multi-carousel/lib/styles.css";
+import ReviewCard from "../ReviewCard/ReviewCard";
+
+const responsive = {
+    desktop: {
+      breakpoint: { max: 3000, min: 1324 },
+      items: 3,
+      slidesToSlide: 1// optional, default to 1.
+    },
+    tablet: {
+      breakpoint: { max: 1324, min: 724 },
+      items: 2,
+      slidesToSlide: 1 // optional, default to 1.
+    },
+    mobile: {
+      breakpoint: { max: 724, min: 0 },
+      items: 1,
+      slidesToSlide: 1 // optional, default to 1.
+    }
+  };
+const Slider = () => {
+  return <Carousel arrows={true} autoPlay={true} autoPlaySpeed={5000} infinite responsive={responsive}>
+    {
+        clientReviews.map(review=>{
+            return <div key={review.image}>
+             {/* review card */}
+             <ReviewCard review={review}/>
+            </div>
+        })
+    }
+  </Carousel>
+};
+
+export default Slider;
